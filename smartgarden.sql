4b17e23d43a092ed8fc9441b0d49d6425ff131e0
Gabriel Raimundo <gabriel.alesiunas@yahoo.com.br>
2023-11-07 08:26:58 -0300

    Banco finalizado com sucesso

--- smartgarden.sql
+++ smartgarden.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 06-Nov-2023 às 21:15
--- Versão do servidor: 8.0.31
--- versão do PHP: 8.1.13
+-- Tempo de geração: 07-Nov-2023 às 10:44
+-- Versão do servidor: 8.0.27
+-- versão do PHP: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,36 +24,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `configuracaoplanta`
---
-
-DROP TABLE IF EXISTS `configuracaoplanta`;
-CREATE TABLE IF NOT EXISTS `configuracaoplanta` (
-  `id` int NOT NULL,
-  `fk_placa_id` int DEFAULT NULL,
-  `data` datetime DEFAULT NULL,
-  `temperatura_ideal` double DEFAULT NULL,
-  `umidade_ideal` double DEFAULT NULL,
-  `luminosidade_ideal` double DEFAULT NULL,
-  `fk_plantas_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `placa`
 --
 
 DROP TABLE IF EXISTS `placa`;
 CREATE TABLE IF NOT EXISTS `placa` (
-  `id` int NOT NULL,
-  `mac` varchar(50) DEFAULT NULL,
-  `fk_usuario_id` int DEFAULT NULL,
-  `descricao` varchar(50) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `mac` varchar(200) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `fk_usuario_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `mac` (`mac`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_usuario_id` (`fk_usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -63,11 +45,11 @@ CREATE TABLE IF NOT EXISTS `placa` (
 
 DROP TABLE IF EXISTS `plantas`;
 CREATE TABLE IF NOT EXISTS `plantas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  `tempideal` int DEFAULT NULL,
-  `umideal` int DEFAULT NULL,
-  `lumideal` int DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `tempideal` bigint DEFAULT NULL,
+  `umideal` bigint DEFAULT NULL,
+  `lumideal` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -81,7 +63,7 @@ DROP TABLE IF EXISTS `produto`;
 CREATE TABLE IF NOT EXISTS `produto` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
-  `descricao` varchar(100) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
   `valor` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -89,19 +71,20 @@ CREATE TABLE IF NOT EXISTS `produto` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `situacaoplanta`
+-- Estrutura da tabela `situacaoplaca`
 --
 
-DROP TABLE IF EXISTS `situacaoplanta`;
-CREATE TABLE IF NOT EXISTS `situacaoplanta` (
-  `id` int NOT NULL,
-  `fk_placa_id` int DEFAULT NULL,
-  `data` datetime DEFAULT NULL,
-  `temperatura` double DEFAULT NULL,
-  `umidade` double DEFAULT NULL,
-  `luminosidade` double DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+DROP TABLE IF EXISTS `situacaoplaca`;
+CREATE TABLE IF NOT EXISTS `situacaoplaca` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `data` date DEFAULT NULL,
+  `temperatura` bigint DEFAULT NULL,
+  `umidade` bigint DEFAULT NULL,
+  `luminosidade` bigint DEFAULT NULL,
+  `fk_placa_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_placa_id` (`fk_placa_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -124,10 +107,24 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `foto_perfil`) VALUES
-(1, 'Gabriel', 'gabriel.alesiunas@yahoo.com.br', '@Ghjk87yzwe99', 'assets/img/imgPerfilIsxisd.jpg'),
-(8, 'admin', 'admin@gmail.com', 'Admin123@', 'assets/img/imgPerfilcorinthians.jpeg'),
-(13, 'a', 'teste@gmail.com', '123', 'assets/img/imgPerfil/userpadrao.png'),
-(14, 'adc', 'd@gmail.com', 'd', 'assets/img/imgPerfil/userpadrao.png');
+(1, 'Gabriel', 'gabriel.alesiunas@yahoo.com.br', 'gabriel123', 'assets/img/imgPerfiluserpadrao.png'),
+(8, 'admin', 'admin@gmail.com', 'Admin123@', 'assets/img/imgPerfilcorinthians.jpeg');
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `placa`
+--
+ALTER TABLE `placa`
+  ADD CONSTRAINT `placa_ibfk_1` FOREIGN KEY (`fk_usuario_id`) REFERENCES `usuario` (`id`);
+
+--
+-- Limitadores para a tabela `situacaoplaca`
+--
+ALTER TABLE `situacaoplaca`
+  ADD CONSTRAINT `situacaoplaca_ibfk_1` FOREIGN KEY (`fk_placa_id`) REFERENCES `placa` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
